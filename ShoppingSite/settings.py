c42d43e8cc0df40bf25f036eb570b72b7c3ebd14
Rushabh Solanki <67600550+Rushabh1221@users.noreply.github.com>
2021-08-05 22:22:18 +0530

    Update settings.py

--- ShoppingSite/settings.py
+++ ShoppingSite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')8wia4)s7r_^eylh6+zgqjiw$)ovscsdr!ufmdubx829krl*z)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
